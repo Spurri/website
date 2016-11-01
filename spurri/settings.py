1a80527f5dc992da58c7fb7c59e7d67c9897f17e
Sean Auriti <sean@alphaonelabs.com>
2016-11-01 17:52:14 -0400

    adds from django.http import Http404

--- spurri/settings.py
+++ spurri/settings.py
@@ -1,6 +1,6 @@
 import os
 import dj_database_url
-
+from django.http import Http404
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
