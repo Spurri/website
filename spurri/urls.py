9d5906be9af59942bd4a68849c5523c608424874
Sean Auriti <sean@alphaonelabs.com>
2016-11-17 22:38:42 -0500

    adding favicon

--- spurri/urls.py
+++ spurri/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import include, url
 from website.views import UserProfileDetailView, ProjectCreate, ProjectDetailView, AddRating, ProjectListView 
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import RedirectView
+
 admin.autodiscover()
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 from django.conf import settings
 from updown.views import AddRatingFromModel
@@ -27,6 +31,7 @@ urlpatterns = [
     url(r"^project/(?P<object_id>\d+)/rate/(?P<score>[\d\-]+)$", AddRating.as_view(), name="project_rating"),
     url(r'^comments/posted/$', website.views.comment_posted, name='comment_posted'),
     url(r'^comments/', include('django_comments.urls')),
+    url(r'^favicon\.ico$', favicon_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
