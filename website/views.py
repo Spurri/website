bf1ee72a5f5acd2293cb51b4c44d95868a46365d
Sean <sean@alphaonelabs.com>
2018-04-28 01:03:43 -0400

    update chart numbers

--- website/views.py
+++ website/views.py
@@ -459,7 +459,7 @@ def sparkline(request, slug):
     import matplotlib.pyplot as plt   
     coin = Cryptocurrency.objects.get(symbol=slug.upper())
 
-    data = list(coin.chart_24h)
+    data = [coin.chart_24h]
 
     fig, ax = plt.subplots(1, 1, figsize=(2, 0.5))
     ax.plot(data)
